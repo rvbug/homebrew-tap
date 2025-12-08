87c69fd600ecf94cd35b805f378743a73a6ba8c5
rvbug <rakesh.venkat@gmail.com>
2025-12-08 08:36:33 +0530

    Fix: URLs

--- Formula/ratcrate-cli.rb
+++ Formula/ratcrate-cli.rb
@@ -6,16 +6,16 @@ class RatcrateCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rvbug/ratcrate-cli/releases/download/v0.1.0/ratcrate-cli-macos-aarch64.tar.gz"
+      url "https://github.com/ratcrate/ratcrate-cli/releases/download/v0.1.0/ratcrate-cli-macos-aarch64.tar.gz"
       sha256 "ab8a31bd54e4ef742362304ba12cc28a996d9c29c9b6e8012f3e25d0d4ec2c1c"
     else
-      url "https://github.com/rvbug/ratcrate-cli/releases/download/v0.1.0/ratcrate-cli-macos-x86_64.tar.gz"
+      url "https://github.com/ratcrate/ratcrate-cli/releases/download/v0.1.0/ratcrate-cli-macos-x86_64.tar.gz"
       sha256 "81b5c872f1cb57d8adb7af94c5970f19bc0376c59d17be0a68196d13707b0469"
     end
   end
 
   on_linux do
-    url "https://github.com/rvbug/ratcrate-cli/releases/download/v0.1.0/ratcrate-cli-linux-x86_64.tar.gz"
+    url "https://github.com/ratcrate/ratcrate-cli/releases/download/v0.1.0/ratcrate-cli-linux-x86_64.tar.gz"
     sha256 "b1f3eb16bd269b430efde8b6c1d46648bedf813b9cf853e52d932d8271642347"
   end
 
